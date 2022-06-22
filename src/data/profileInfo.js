d7664b5a44c1708462d86c77d0b7ee7519281edb
VincentChen6345 <vincent.chen6345@hotmail.com>
2022-06-22 20:56:19 +1000

    props

---
+++ src/data/profileInfo.js
@@ -0,0 +1,10 @@
+const profileInfo = {
+  name: "Vincent Chen",
+  age: 25,
+  height: 173,
+  job: "aspiring front end dev",
+  language: "javascript",
+  "Favourite Food": "Chicken burger with fries",
+};
+
+export default profileInfo;
